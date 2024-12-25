ca85f52423c554e313c03486eaef8e3c7bd4aca5
Irish Micole Cando <irishmicolecando@gmail.com>
2024-12-25 16:12:54 +0800

    chore: add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,8 +13,21 @@ export default {
         'mobile-light': "url(/bg-mobile-light.jpg)"
       },
       colors: {
-        'light-gray': '#F5F9EA',
-        'dark-gray': '#454545'
+        'bright-blue': 'hsl(220, 98%, 61%)',
+        // light theme
+        'very-light-gray': 'hsl(0, 0%, 98%)',
+        'very-light-grayish-blue': 'hsl(236, 33%, 92%)',
+        'light-grayish-blue-light': 'hsl(233, 11%, 84%)',
+        'dark-grayish-blue-light': 'hsl(236, 9%, 61%)',
+        'very-dark-grayish-blue-light': 'hsl(235, 19%, 35%)',
+        // dark theme
+        'very-dark-blue': 'hsl(235, 21%, 11%)',
+        'very-dark-desaturated-blue': 'hsl(235, 24%, 19%)',
+        'light-grayish-blue-dark': 'hsl(234, 39%, 85%)',
+        'light-grayish-blue-dark-hover': 'hsl(236, 33%, 92%)',
+        'dark-grayish-blue-dark': 'hsl(234, 11%, 52%)',
+        'very-dark-grayish-blue-dark': 'hsl(233, 14%, 35%)',
+        'very-dark-grayish-blue-dark-hover': 'hsl(237, 14%, 26%)'
       },
       fontFamily: {
         'josefin-sans': ['Josefin Sans', 'serif']
