60e8d7bd5a8301d38cae3c61cba09a7e432a6567
Irish Micole Cando <irishmicolecando@gmail.com>
2024-12-26 08:20:48 +0800

    chore: remove Supabase dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,13 @@
-import { useState, useEffect } from 'react'
-import { Auth } from '@supabase/auth-ui-react'
-import { ThemeSupa } from '@supabase/auth-ui-shared'
+import { useState } from 'react'
 import { ToDo } from './pages/ToDo'
 import { ToDoList } from './context/ToDoListContext'
-import { supabase } from './services/supabaseClient'
 
 export default function App() {
-  const [session, setSession] = useState(null)
   const [todoList, setToDoList] = useState([])
 
-  useEffect(() => {
-    supabase.auth.getSession().then(({ data: { session } }) => {
-      setSession(session)
-    })
-
-    const {
-      data: { subscription },
-    } = supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session)
-    })
-
-    return () => subscription.unsubscribe()
-  }, [])
-
-  if (!session) {
-    return (<Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />)
-  }
-  else {
-    return (
-      <ToDoList.Provider value={{todoList, setToDoList }}>
-        <ToDo />
-      </ToDoList.Provider>
-    )
-  }
+  return (
+    <ToDoList.Provider value={{todoList, setToDoList }}>
+      <ToDo />
+    </ToDoList.Provider>
+  )
 }
