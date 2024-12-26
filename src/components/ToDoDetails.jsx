945a87f1f653a713f3fdaebb8cc8716d47bcbaa7
Irish Micole Cando <irishmicolecando@gmail.com>
2024-12-27 05:53:35 +0800

    feat: add mobile design on task filter

--- src/components/ToDoDetails.jsx
+++ src/components/ToDoDetails.jsx
@@ -9,8 +9,8 @@ export const ToDoDetails = () => {
   }
 
   return (
-    <div className="flex flex-row justify-between px-6 py-4 text-sm  font-bold text-gray-500">
-      <p>{filteredTodos.length} items left</p>
+    <div className="flex flex-row justify-between px-6 py-4 text-sm font-bold text-gray-500">
+      <p className="font-semibold">{filteredTodos.length} items left</p>
       <div className="hidden sm:flex flex-row space-x-3 hover:cursor-pointer">
         <button
           className={`${filter === 'all' ? 'text-bright-blue' : 'text-gray-500 hover:text-white'}`}
@@ -31,7 +31,7 @@ export const ToDoDetails = () => {
           Completed
         </button>
       </div>
-      <button className="hover:text-white" onClick={clearCompleted}>Clear Completed</button>
+      <button className="hover:text-white font-semi-bold" onClick={clearCompleted}>Clear Completed</button>
     </div>
   )
 }
--- src/pages/ToDo.jsx
+++ src/pages/ToDo.jsx
@@ -1,22 +1,49 @@
+import { useContext } from "react";
 import { CreateToDo } from "../components/CreateToDo"
 import { ToDoDetails } from "../components/ToDoDetails"
 import { TodoElement } from "../components/TodoElement"
 import { ToDoListHeader } from "../components/ToDoListHeader"
+import { ToDoList } from "../context/ToDoListContext";
 
 export const ToDo = () => {
+  const { filter, setFilter } = useContext(ToDoList)
+
   return (
-    <div className="font-josefin-sans light:bg-gray-300 dark:bg-very-dark-blue h-screen">
-      <div className="bg-desktop-dark bg-cover bg-no-repeat h-[320px] w-full flex items-center justify-center">
-        
-      </div>
-      <div className="absolute top-0 left-1/2 transform -translate-x-1/2 flex flex-col sm:w-full max-w-2xl mt-20">
+    <div className="font-josefin-sans light:bg-gray-300 dark:bg-very-dark-blue min-h-screen">
+      <div className="bg-mobile-dark md:bg-desktop-dark bg-cover bg-no-repeat h-[200px] md:h-[320px] w-full" />
+      <div className="absolute top-0 left-0 right-0 px-4 md:px-0 flex flex-col items-center w-full">
+        <div className="w-full max-w-md md:max-w-2xl mt-10 md:mt-20">
           <ToDoListHeader />
-          <CreateToDo />
-          <div className="w-full max-w-2xl mx-auto bg-very-light-gray dark:bg-very-dark-desaturated-blue rounded-lg">
-            <TodoElement />
-            <ToDoDetails />
+          <div className="space-y-10">
+            <CreateToDo />
+            <div className="w-full bg-very-light-gray dark:bg-very-dark-desaturated-blue rounded-lg shadow-lg overflow-hidden">
+              <TodoElement />
+              <ToDoDetails />
+            </div>
           </div>
         </div>
+        <div className="sm:hidden flex flex-row justify-center space-x-3 mt-5 p-4 w-full max-w-md bg-very-light-gray dark:bg-very-dark-desaturated-blue rounded-lg shadow-lg overflow-hidden hover:cursor-pointer font-bold text-gray-500">
+          <button
+            className={`${filter === 'all' ? 'text-bright-blue' : 'text-gray-500 hover:text-white'}`}
+            onClick={() => setFilter('all')}
+          >
+            All
+          </button>
+          <button 
+            onClick={() => setFilter('active')} 
+            className={`${filter === 'active' ? 'text-bright-blue' : 'text-gray-500 hover:text-white'}`}
+          >
+            Active
+          </button>
+          <button 
+            onClick={() => setFilter('completed')} 
+            className={`${filter === 'completed' ? 'text-bright-blue' : 'text-gray-500 hover:text-white'}`}
+          >
+            Completed
+          </button>
+        </div>
+        <div className="h-16 md:h-0" />
+      </div>
     </div>
   )
 }
